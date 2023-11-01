1016069601ba498e2fc73f922a37bfdd18bd38ec
MahiruShN <minkuan1704@gmail.com>
2023-11-02 00:03:48 +0700

    game loop

--- Java2dGame/src/com/kuan/game/GamePanel.java
+++ Java2dGame/src/com/kuan/game/GamePanel.java
@@ -1,4 +1,6 @@
 package com.kuan.game;
+import org.ietf.jgss.GSSManager;
+
 import  javax.swing.JPanel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -36,7 +38,13 @@ public class GamePanel extends JPanel implements Runnable{
     }
     public void run() {
         init();
-
+        final double GAME_HERTZ = 60.0;
+        final double TBU = 1000000000 / GAME_HERTZ;
+        final int MUBR = 5;
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+        final double TARGET_FPS = 60.0;
+        final  double TTBR = 1000000000 / TARGET_FPS ;
         while (running) {
             update();
             render();
@@ -49,9 +57,12 @@ public class GamePanel extends JPanel implements Runnable{
         x++;
     }
     public  void render() {
-
+        g.setColor( new Color(66, 131, 168));
+        g.fillRect(0,0,width,height);
     }
     public void draw() {
-
+        Graphics g2 = (Graphics) this.getGraphics();
+        g2.drawImage( img,0,0, width, height,null);
+        g2.dispose();
     }
 }
