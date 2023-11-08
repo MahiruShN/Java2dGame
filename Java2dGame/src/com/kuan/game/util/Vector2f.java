96ee1a505caf5d8e06cd423d5e75d28775d14557
MahiruShN <minkuan1704@gmail.com>
2023-11-09 05:21:00 +0700

    fix ;

--- Java2dGame/src/com/kuan/game/util/Vector2f.java
+++ Java2dGame/src/com/kuan/game/util/Vector2f.java
@@ -40,7 +40,7 @@ public class Vector2f {
     }
 
     public Vector2f getWorldVar() {
-        return new Vector2f(x - worldX, y - worldY)
+        return new Vector2f(x - worldX, y - worldY);
     }
 
     public String toString() {
